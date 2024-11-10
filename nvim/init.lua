80dbf9d6bf128a21723faea19d7e4f2b25e96985
MrConnorKenway <auriaiur@outlook.com>
2024-11-10 21:54:50 +0800

    Update
    - optimize conform.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -281,12 +281,30 @@ require('lazy').setup({
   {
     'stevearc/conform.nvim',
     event = 'InsertEnter',
+    keys = {
+      {
+        '<leader>f',
+        mode = 'v',
+        function()
+          require('conform').format({ async = true }, function(err)
+            if not err then
+              local mode = vim.api.nvim_get_mode().mode
+              if vim.startswith(string.lower(mode), 'v') then
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+              end
+            end
+          end)
+        end,
+        desc = 'LSP range format'
+      },
+    },
     config = function()
       require('conform').setup({
         formatters_by_ft = {
           c = { 'clang-format' }
         },
-        format_after_save = {
+        format_on_save = {
+          timeout_ms = 500,
           lsp_format = 'fallback'
         }
       })
