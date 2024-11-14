256d000a1b510bcef4f35349164a97eec9312e14
MrConnorKenway <auriaiur@outlook.com>
2024-11-14 21:17:50 +0800

    Add OSC52 remote copy support
    - note: neovim's nightly release natively supports this feature

--- nvim/init.lua
+++ nvim/init.lua
@@ -138,6 +138,37 @@ require('lazy').setup({
     }
   },
   {
+    'ojroques/nvim-osc52',
+    config = function()
+      if os.getenv('SSH_TTY') == nil then return end
+
+      require('osc52').setup()
+
+      local function copy(lines, _)
+        require('osc52').copy(table.concat(lines, '\n'))
+      end
+
+      local function paste()
+        return {
+          vim.fn.split(vim.fn.getreg(''), '\n'),
+          vim.fn.getregtype('')
+        }
+      end
+
+      vim.g.clipboard = {
+        name = 'osc52',
+        copy = {
+          ['+'] = copy,
+          ['*'] = copy
+        },
+        paste = {
+          ['+'] = paste,
+          ['*'] = paste
+        }
+      }
+    end
+  },
+  {
     'RRethy/vim-illuminate',
     config = function()
       require('illuminate').configure {
