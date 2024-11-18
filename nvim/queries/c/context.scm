3db9065a654913a4e0dddcfede768366cd7eb4c2
MrConnorKenway <auriaiur@outlook.com>
2024-11-18 16:21:27 +0800

    WIP
    - TODO: missing highlight
    - more queries needs to be config

---
+++ nvim/queries/c/context.scm
@@ -0,0 +1,19 @@
+(function_definition
+  body: (_ (_) @context.end)
+) @context
+
+(struct_specifier
+  body: (_ (_) @context.end)
+) @context
+
+(enum_specifier
+  body: (_ (_) @context.end)
+) @context
+
+(switch_statement
+  body: (_) @context.end
+) @context
+
+(case_statement
+  value: (_) (_) @context.end
+) @context
