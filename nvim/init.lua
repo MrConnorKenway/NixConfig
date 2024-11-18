d6dbfae522dfe42298d59785cd3a3d8eda653f03
MrConnorKenway <auriaiur@outlook.com>
2024-11-18 19:28:10 +0800

    Add nvim-navic to display code context in statusline

--- nvim/init.lua
+++ nvim/init.lua
@@ -504,6 +504,55 @@ require('lazy').setup({
     config = true
   },
   {
+    'SmiteshP/nvim-navic',
+    config = function()
+      require('nvim-navic').setup {
+        icons = {
+          File = ' ',
+          Module = ' ',
+          Namespace = ' ',
+          Package = ' ',
+          Class = ' ',
+          Method = ' ',
+          Property = ' ',
+          Field = ' ',
+          Constructor = ' ',
+          Enum = ' ',
+          Interface = ' ',
+          Function = '󰊕 ',
+          Variable = '󰫧 ',
+          Constant = ' ',
+          String = ' ',
+          Number = ' ',
+          Boolean = ' ',
+          Array = ' ',
+          Object = ' ',
+          Key = ' ',
+          Null = ' ',
+          EnumMember = ' ',
+          Struct = ' ',
+          Event = ' ',
+          Operator = ' ',
+          TypeParameter = ' '
+        },
+        lsp = {
+          auto_attach = true,
+          preference = nil,
+        },
+        highlight = true,
+        separator = ' > ',
+        depth_limit = 0,
+        depth_limit_indicator = '..',
+        safe_output = true,
+        lazy_update_context = false,
+        click = false,
+        format_text = function(text)
+          return text
+        end,
+      }
+    end
+  },
+  {
     'neovim/nvim-lspconfig',
     config = function()
       vim.api.nvim_create_autocmd('LspAttach', {
--- nvim/lua/heirline/config.lua
+++ nvim/lua/heirline/config.lua
@@ -2,7 +2,7 @@ return {
   'rebelot/heirline.nvim',
   lazy = false,
   priority = 999,
-  dependencies = 'linrongbin16/lsp-progress.nvim',
+  dependencies = { 'linrongbin16/lsp-progress.nvim', 'SmiteshP/nvim-navic' },
   config = function()
     local conditions = require('heirline.conditions')
     local utils = require('heirline.utils')
@@ -349,6 +349,7 @@ return {
 
     local Align = { provider = '%=' }
     local Space = { provider = ' ' }
+    local LongSpace = { provider = '  ' }
 
     local SpecialStatusline = {
       condition = function()
@@ -389,12 +390,95 @@ return {
       hl = { fg = theme_colors.green, bold = true },
     }
 
+    local Navic = {
+      condition = function() return require('nvim-navic').is_available() end,
+      static = {
+        -- create a type highlight map
+        type_hl = {
+          File = 'Directory',
+          Module = '@include',
+          Namespace = '@namespace',
+          Package = '@include',
+          Class = '@structure',
+          Method = '@method',
+          Property = '@property',
+          Field = '@field',
+          Constructor = '@constructor',
+          Enum = '@field',
+          Interface = '@type',
+          Function = '@function',
+          Variable = '@variable',
+          Constant = '@constant',
+          String = '@string',
+          Number = '@number',
+          Boolean = '@boolean',
+          Array = '@field',
+          Object = '@type',
+          Key = '@keyword',
+          Null = '@comment',
+          EnumMember = '@field',
+          Struct = '@structure',
+          Event = '@keyword',
+          Operator = '@operator',
+          TypeParameter = '@type',
+        },
+        -- bit operation dark magic, see below...
+        enc = function(line, col, winnr)
+          return bit.bor(bit.lshift(line, 16), bit.lshift(col, 6), winnr)
+        end,
+        -- line: 16 bit (65535); col: 10 bit (1023); winnr: 6 bit (63)
+        dec = function(c)
+          local line = bit.rshift(c, 16)
+          local col = bit.band(bit.rshift(c, 6), 1023)
+          local winnr = bit.band(c, 63)
+          return line, col, winnr
+        end
+      },
+      init = function(self)
+        local data = require('nvim-navic').get_data() or {}
+        local children = {}
+        -- create a child for each level
+        for i, d in ipairs(data) do
+          -- encode line and column numbers into a single integer
+          local child = {
+            {
+              provider = d.icon,
+              hl = self.type_hl[d.type],
+            },
+            {
+              -- escape `%`s (elixir) and buggy default separators
+              provider = d.name:gsub('%%', '%%%%'):gsub('%s*->%s*', ''),
+              -- highlight icon only or location name as well
+              hl = self.type_hl[d.type],
+            }
+          }
+          -- add a separator only if needed
+          if #data > 1 and i < #data then
+            table.insert(child, {
+              provider = ' > ',
+              hl = { fg = 'bright_fg' },
+            })
+          end
+          table.insert(children, child)
+        end
+        -- instantiate the new child, overwriting the previous one
+        self.child = self:new(children, 1)
+      end,
+      -- evaluate the children containing navic components
+      provider = function(self)
+        return self.child:eval()
+      end,
+      update = 'CursorMoved'
+    }
+
     local DefaultStatusline = {
       ViMode,
       Space,
       FileNameBlock,
       Space,
       Git,
+      LongSpace,
+      Navic,
       Align,
       Diagnostics,
       Space,
@@ -421,6 +505,8 @@ return {
         FileNameBlock,
         Space,
         Git,
+        LongSpace,
+        Navic,
         Align,
         Diagnostics,
         Space,
