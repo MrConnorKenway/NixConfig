1190519ec89c3878897f4e8980bb2cbdd628a8b8
MrConnorKenway <auriaiur@outlook.com>
2024-11-18 17:34:18 +0800

    Tweak auto-session
    - no telescope loading on setup
    - add shortcut to perform session search

--- nvim/init.lua
+++ nvim/init.lua
@@ -311,7 +311,13 @@ require('lazy').setup({
     'rmagatti/auto-session',
     lazy = false,
     config = function()
-      require('auto-session').setup {}
+      require('auto-session').setup {
+        session_lens = {
+          load_on_setup = false
+        }
+      }
+
+      vim.keymap.set('n', '<leader>j', '<cmd>SessionSearch<cr>', { desc = 'Session Search' })
     end
   },
   {
