cc12c193a2ab556450d3c52012c0a4bb48656656
MrConnorKenway <auriaiur@outlook.com>
2024-11-26 19:22:43 +0800

    Use arrow.nvim to pin and navigate buffers

--- nvim/init.lua
+++ nvim/init.lua
@@ -423,6 +423,24 @@ require('lazy').setup({
     end
   },
   {
+    'otavioschwanck/arrow.nvim',
+    event = 'VimEnter',
+    keys = {
+      { '<C-S-n>', function() require('arrow.persist').next() end, desc = 'Move to next arrow buffer' },
+      { '<C-S-p>', function() require('arrow.persist').previous() end, desc = 'Move to previous arrow buffer' }
+    },
+    dependencies = {
+      { 'nvim-tree/nvim-web-devicons' },
+    },
+    opts = {
+      show_icons = true,
+      leader_key = 'm',
+      window = {
+        border = 'rounded'
+      }
+    }
+  },
+  {
     'dgagn/diagflow.nvim',
     event = 'LspAttach',
     config = function()
