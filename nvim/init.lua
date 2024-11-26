e9c3e639193a093ecf156a1a3a96ef5b5ff756ba
MrConnorKenway <auriaiur@outlook.com>
2024-11-26 17:37:41 +0800

    Better lsp diagnostic message
    - display multiple diagnostic message of same line
    - won't display if cursor is not moved to the problematical location

--- nvim/init.lua
+++ nvim/init.lua
@@ -421,6 +421,46 @@ require('lazy').setup({
     end
   },
   {
+    'dgagn/diagflow.nvim',
+    event = 'LspAttach',
+    config = function()
+      require('diagflow').setup({
+        enable = true,
+        max_width = 60,     -- The maximum width of the diagnostic messages
+        max_height = 10,    -- the maximum height per diagnostics
+        severity_colors = { -- The highlight groups to use for each diagnostic severity level
+          error = "DiagnosticFloatingError",
+          warning = "DiagnosticFloatingWarn",
+          info = "DiagnosticFloatingInfo",
+          hint = "DiagnosticFloatingHint",
+        },
+        format = function(diagnostic)
+          return diagnostic.message
+        end,
+        gap_size = 1,
+        scope = 'cursor',
+        padding_top = 1,
+        padding_right = 0,
+        text_align = 'right',                                  -- 'left', 'right'
+        placement = 'top',                                     -- 'top', 'inline'
+        inline_padding_left = 0,                               -- the padding left when the placement is inline
+        update_event = { 'DiagnosticChanged', 'BufReadPost' }, -- the event that updates the diagnostics cache
+        toggle_event = { 'InsertEnter', 'InsertLeave' },       -- if InsertEnter, can toggle the diagnostics on inserts
+        show_sign = false,
+        render_event = { 'DiagnosticChanged', 'CursorMoved' },
+        border_chars = {
+          top_left = '╭',
+          top_right = '╮',
+          bottom_left = '╰',
+          bottom_right = '╯',
+          horizontal = '─',
+          vertical = '│'
+        },
+        show_borders = true,
+      })
+    end
+  },
+  {
     'stevearc/dressing.nvim',
     event = 'UIEnter'
   },
