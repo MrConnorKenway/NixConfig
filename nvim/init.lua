d133502302f5ea09e7a88fad4b8123dd7efb2315
MrConnorKenway <auriaiur@outlook.com>
2024-11-27 15:39:53 +0800

    Change the keymaps of `smart-splits.nvim' to use lua API

--- nvim/init.lua
+++ nvim/init.lua
@@ -841,14 +841,54 @@ require('lazy').setup({
   {
     'mrjones2014/smart-splits.nvim',
     keys = {
-      { '<C-h>', mode = { 'n', 'i', 't' }, '<cmd>SmartCursorMoveLeft<cr>',  desc = 'Move cursort left' },
-      { '<C-j>', mode = { 'n', 'i', 't' }, '<cmd>SmartCursorMoveDown<cr>',  desc = 'Move cursort down' },
-      { '<C-k>', mode = { 'n', 'i', 't' }, '<cmd>SmartCursorMoveUp<cr>',    desc = 'Move cursort up' },
-      { '<C-l>', mode = { 'n', 'i', 't' }, '<cmd>SmartCursorMoveRight<cr>', desc = 'Move cursort right' },
-      { '<M-h>', mode = { 'n', 'i', 't' }, '<cmd>SmartResizeLeft<cr>',      desc = 'Resize window left' },
-      { '<M-j>', mode = { 'n', 'i', 't' }, '<cmd>SmartResizeDown<cr>',      desc = 'Resize window down' },
-      { '<M-k>', mode = { 'n', 'i', 't' }, '<cmd>SmartResizeUp<cr>',        desc = 'Resize window up' },
-      { '<M-l>', mode = { 'n', 'i', 't' }, '<cmd>SmartResizeRight<cr>',     desc = 'Resize window right' }
+      {
+        '<C-h>',
+        mode = { 'n', 'i', 't' },
+        function() require('smart-splits').move_cursor_left() end,
+        desc = 'Move cursort left'
+      },
+      {
+        '<C-j>',
+        mode = { 'n', 'i', 't' },
+        function() require('smart-splits').move_cursor_down() end,
+        desc = 'Move cursort down'
+      },
+      {
+        '<C-k>',
+        mode = { 'n', 'i', 't' },
+        function() require('smart-splits').move_cursor_up() end,
+        desc = 'Move cursort up'
+      },
+      {
+        '<C-l>',
+        mode = { 'n', 'i', 't' },
+        function() require('smart-splits').move_cursor_right() end,
+        desc = 'Move cursort right'
+      },
+      {
+        '<M-h>',
+        mode = { 'n', 'i', 't' },
+        function() require('smart-splits').resize_left() end,
+        desc = 'Resize window left'
+      },
+      {
+        '<M-j>',
+        mode = { 'n', 'i', 't' },
+        function() require('smart-splits').resize_down() end,
+        desc = 'Resize window down'
+      },
+      {
+        '<M-k>',
+        mode = { 'n', 'i', 't' },
+        function() require('smart-splits').resize_up() end,
+        desc = 'Resize window up'
+      },
+      {
+        '<M-l>',
+        mode = { 'n', 'i', 't' },
+        function() require('smart-splits').resize_right() end,
+        desc = 'Resize window right'
+      }
     },
     config = function()
       require('smart-splits').setup {
