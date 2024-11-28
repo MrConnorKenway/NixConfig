6198c4241450f4f2e721592ed7c53fadb521095b
MrConnorKenway <auriaiur@outlook.com>
2024-11-28 23:24:39 +0800

    Introduce tabout.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -624,6 +624,38 @@ require('lazy').setup({
     end
   },
   {
+    'abecodes/tabout.nvim',
+    config = function()
+      require('tabout').setup {
+        tabkey = '<Tab>',             -- key to trigger tabout, set to an empty string to disable
+        backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+        act_as_tab = true,            -- shift content if tab out is not possible
+        act_as_shift_tab = false,     -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+        default_tab = '<C-t>',        -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+        default_shift_tab = '<C-d>',  -- reverse shift default action,
+        enable_backwards = true,      -- well ...
+        completion = false,           -- if the tabkey is used in a completion pum
+        tabouts = {
+          { open = "'", close = "'" },
+          { open = '"', close = '"' },
+          { open = '`', close = '`' },
+          { open = '(', close = ')' },
+          { open = '[', close = ']' },
+          { open = '<', close = '>' },
+          { open = '{', close = '}' }
+        },
+        ignore_beginning = false, -- only tabout if at beginning of configured tabout chars
+        exclude = {}              -- tabout will ignore these filetypes
+      }
+    end,
+    dependencies = { -- These are optional
+      'nvim-treesitter/nvim-treesitter',
+      'hrsh7th/nvim-cmp'
+    },
+    opt = true, -- Set this to true if the plugin is optional
+    event = 'InsertEnter'
+  },
+  {
     'hrsh7th/nvim-cmp',
     dependencies = {
       'hrsh7th/cmp-nvim-lsp',
