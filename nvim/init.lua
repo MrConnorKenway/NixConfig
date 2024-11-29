fdfa697257e0df792a496966e814bfa415fc3f0e
MrConnorKenway <auriaiur@outlook.com>
2024-11-29 14:38:05 +0800

    Introduce blink.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -419,6 +419,13 @@ require('lazy').setup({
     lazy = false,
     init = function()
       vim.cmd.colorscheme('catppuccin-mocha')
+    end,
+    config = function()
+      require('catppuccin').setup {
+        integrations = {
+          blink_cmp = true
+        }
+      }
     end
   },
   {
@@ -628,40 +635,93 @@ require('lazy').setup({
     end
   },
   {
+    'saghen/blink.cmp',
+    lazy = false, -- lazy loading handled internally
+    -- optional: provides snippets for the snippet source
+    dependencies = 'rafamadriz/friendly-snippets',
+
+    -- use a release tag to download pre-built binaries
+    version = 'v0.*',
+    -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    opts = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- see the "default configuration" section below for full documentation on how to define
+      -- your own keymap.
+      keymap = {
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
+        ['<Tab>'] = {
+          function(cmp)
+            if cmp.is_in_snippet() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          'snippet_forward',
+          'fallback'
+        },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      },
+
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release
+        use_nvim_cmp_as_default = false,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      -- default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, via `opts_extend`
+      sources = {
+        completion = {
+          enabled_providers = { 'lsp', 'path', 'snippets' },
+        },
+      },
+
+      windows = {
+        autocomplete = {
+          cycle = { from_top = false }, -- cycle at bottom, but not at the top
+          border = 'rounded'
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 250,
+          border = 'rounded'
+        },
+        signature_help = {
+          border = 'rounded' -- TODO: border not changable
+        }
+      },
+    },
+    -- allows extending the enabled_providers array elsewhere in your config
+    -- without having to redefine it
+    opts_extend = { "sources.completion.enabled_providers" }
+  },
+  {
     'hrsh7th/nvim-cmp',
     dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-cmdline'
     },
     event = { 'InsertEnter', 'CmdlineEnter' },
     config = function()
       local cmp = require('cmp')
-      cmp.setup {
-        sources = {
-          { name = 'nvim_lsp' }
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered()
-        },
-        mapping = cmp.mapping.preset.insert {
-          ['<cr>'] = cmp.mapping.confirm { select = true },
-          ['<tab>'] = cmp.mapping.confirm { select = true },
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-p>'] = cmp.mapping.select_prev_item()
-        },
-        matching = {
-          disallow_partial_matching = false,
-          disallow_prefix_unmatching = true,
-          disallow_fuzzy_matching = true,
-          disallow_fullfuzzy_matching = true,
-          disallow_partial_fuzzy_matching = true,
-          disallow_symbol_nonprefix_matching = true
-        }
-      }
       cmp.setup.cmdline(':', {
         sources = {
           { name = 'cmdline' }
