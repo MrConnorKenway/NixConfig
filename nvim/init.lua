d30f414f4b0eb07f1631cb2080fa130490294b64
MrConnorKenway <auriaiur@outlook.com>
2024-11-30 10:26:37 +0800

    Fallback to builtin '[c' ']c' when in diff mode

--- nvim/init.lua
+++ nvim/init.lua
@@ -250,8 +250,20 @@ require('lazy').setup({
           border = 'rounded'
         }
       }
-      vim.keymap.set('n', ']c', function() gitsigns.nav_hunk('next') end, { desc = 'Go to next git change' })
-      vim.keymap.set('n', '[c', function() gitsigns.nav_hunk('prev') end, { desc = 'Go to previous git change' })
+      vim.keymap.set('n', ']c', function()
+        if vim.api.nvim_buf_get_name(0):match('fugitive://') then
+          vim.cmd('normal! ]c | zz')
+        else
+          gitsigns.nav_hunk('next')
+        end
+      end, { desc = 'Go to next git change' })
+      vim.keymap.set('n', '[c', function()
+        if vim.api.nvim_buf_get_name(0):match('fugitive://') then
+          vim.cmd('normal! [c | zz')
+        else
+          gitsigns.nav_hunk('prev')
+        end
+      end, { desc = 'Go to previous git change' })
       vim.keymap.set('n', '<leader>u', gitsigns.reset_hunk, { desc = 'Git reset hunk' })
       vim.keymap.set('n', '<leader>b', gitsigns.blame_line, { desc = 'Git blame inline' })
       vim.keymap.set('n', 'ga', gitsigns.stage_hunk, { desc = 'Git stage hunk' })
