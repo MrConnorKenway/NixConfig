447440af5a001647b0f4009bbad61a28649b00ef
MrConnorKenway <auriaiur@outlook.com>
2024-12-02 16:41:56 +0800

    Add shortcut to format whole buffer

--- nvim/init.lua
+++ nvim/init.lua
@@ -905,6 +905,36 @@ require('lazy').setup({
         end,
         desc = 'LSP range format'
       },
+      {
+        '<S-D-i>',
+        mode = { 'n', 'i' },
+        function()
+          require('conform').format({ async = true }, function(err)
+            if not err then
+              local mode = vim.api.nvim_get_mode().mode
+              if vim.startswith(string.lower(mode), 'v') then
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+              end
+            end
+          end)
+        end,
+        desc = 'LSP format current buffer'
+      },
+      {
+        '<S-M-i>',
+        mode = { 'n', 'i' },
+        function()
+          require('conform').format({ async = true }, function(err)
+            if not err then
+              local mode = vim.api.nvim_get_mode().mode
+              if vim.startswith(string.lower(mode), 'v') then
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+              end
+            end
+          end)
+        end,
+        desc = 'LSP format current buffer'
+      }
     },
     config = function()
       require('conform').setup({
