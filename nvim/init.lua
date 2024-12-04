e8372ea197daa64c333cbb8775c881dbbb341ac6
MrConnorKenway <auriaiur@outlook.com>
2024-12-04 17:35:42 +0800

    Use <C-s> to escape terminal mode for all terminals (not just here.term)

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,6 +54,8 @@ vim.keymap.set({ 'i', 'n' }, '<D-s>', '<cmd>wa<cr>', { desc = 'Save workspace wi
 vim.keymap.set('n', '<leader>x', '<cmd>xa<cr>', { desc = 'Save and quit workspace' })
 vim.keymap.set('n', '<leader>q', '<cmd>qa<cr>', { desc = 'Quit workspace without save' })
 vim.keymap.set('v', '<leader>c', '"+y', { desc = 'OSC52 copy' })
+vim.keymap.set('t', '<C-s>', vim.api.nvim_replace_termcodes('<C-\\><C-N>', true, true, true),
+  { silent = true, desc = 'Exit terminal mode' })
 
 -- copy from https://github.com/neovim/neovim/pull/28176/files#diff-49225a49c226c2f1b36f966d0178c556e204cdc0b660c80db9e4568e03f6ef99R126
 -- WARN: may change as neovim updates
@@ -653,8 +655,7 @@ require('lazy').setup({
           enable = false
         },
         extra_mappings = {
-          enable = true,
-          escape = '<C-s>'
+          enable = false
         }
       }
     end
