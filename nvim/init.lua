67e4cdccfc1dae62b8d10993af83ed58fae58a1b
MrConnorKenway <auriaiur@outlook.com>
2024-12-04 17:46:06 +0800

    Use lua keybindings for vim-dispatch

--- nvim/init.lua
+++ nvim/init.lua
@@ -355,7 +355,17 @@ require('lazy').setup({
     },
     event = 'CmdlineEnter'
   },
-  { 'tpope/vim-dispatch', },
+  {
+    'tpope/vim-dispatch',
+    keys = {
+      { '`<Space>', ':Dispatch ',     desc = 'Dispatch command to run and return results in quickfix' },
+      { "'<Space>", ':Start ',        desc = 'Start an eval process in a new focused window' },
+      { '``',       '<cmd>Start<cr>', desc = 'Start a shell in a new focused window' },
+    },
+    init = function()
+      vim.g.dispatch_no_maps = 1
+    end
+  },
   {
     'folke/flash.nvim',
     event = 'VeryLazy',
