979e625239a9a34be167e9e25b7bdc17e1e7220f
MrConnorKenway <auriaiur@outlook.com>
2025-01-04 01:55:54 +0800

    Revert part of d4c38ab

--- nvim/init.lua
+++ nvim/init.lua
@@ -423,18 +423,20 @@ require('lazy').setup({
   },
   {
     'nvim-treesitter/nvim-treesitter',
-    opts = {
-      modules = {},
-      ignore_install = {},
-      auto_install = false,
-      sync_install = false,
-      highlight = { enable = false },
-      indent = { enable = false },
-      ensure_installed = {
-        'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline',
-        'nix', 'asm', 'cpp', 'make', 'python', 'bash', 'rust', 'zig'
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        modules = {},
+        ignore_install = {},
+        auto_install = false,
+        sync_install = false,
+        highlight = { enable = false },
+        indent = { enable = false },
+        ensure_installed = {
+          'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline',
+          'nix', 'asm', 'cpp', 'make', 'python', 'bash', 'rust', 'zig'
+        }
       }
-    }
+    end
   },
   {
     'catppuccin/nvim',
