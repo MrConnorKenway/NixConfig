4e32b258fda347751ed81f65f66aea72af5ee899
MrConnorKenway <auriaiur@outlook.com>
2025-01-14 11:50:50 +0800

    Load tiny-inline-diagnostic earlier at UIEnter to avoid not displaying

--- nvim/lua/plugins/ui/tiny-inline-diagnostic.lua
+++ nvim/lua/plugins/ui/tiny-inline-diagnostic.lua
@@ -1,6 +1,6 @@
 return {
   'rachartier/tiny-inline-diagnostic.nvim',
-  event = 'VeryLazy',
+  event = 'UIEnter',
   config = function()
     vim.diagnostic.config({ virtual_text = false })
     require('tiny-inline-diagnostic').setup {
