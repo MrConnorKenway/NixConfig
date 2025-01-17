6105e4d9c328096f7dae90e2a66ffdce0f19f2d4
MrConnorKenway <auriaiur@outlook.com>
2025-01-17 11:24:15 +0800

    Use `require` instead of global `Snacks` to remove warning

--- nvim/lua/plugins/ui/snacks.lua
+++ nvim/lua/plugins/ui/snacks.lua
@@ -3,8 +3,8 @@ return {
   priority = 1000,
   lazy = false,
   keys = {
-    { '<leader>n', function() Snacks.notifier.show_history() end, desc = 'Notification History' },
-    { '<leader>c', function() Snacks.bufdelete.delete() end,      desc = 'Close buffer' }
+    { '<leader>n', function() require('snacks.notifier').show_history() end, desc = 'Notification History' },
+    { '<leader>c', function() require('snacks.bufdelete').delete() end,      desc = 'Close buffer' }
   },
   opts = {
     notifier = {
