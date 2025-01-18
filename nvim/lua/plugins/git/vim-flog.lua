31c3cc722715e1c91f8764e346d49c11fd0ed0ad
MrConnorKenway <auriaiur@outlook.com>
2025-01-18 14:22:09 +0800

    Display Flog in new tab page since we use tabby

--- nvim/lua/plugins/git/vim-flog.lua
+++ nvim/lua/plugins/git/vim-flog.lua
@@ -3,10 +3,9 @@ return {
   init = function()
     vim.g.flog_enable_extended_chars = true
   end,
-  cmd = { 'Flog', 'Flogsplit', 'Floggit' },
   keys = {
-    { '<S-M-l>', function() vim.cmd('vertical Flogsplit') end, desc = 'Display git graph' },
-    { '<S-D-l>', function() vim.cmd('vertical Flogsplit') end, desc = 'Display git graph' }
+    { '<S-M-l>', '<cmd>Flog<cr>', desc = 'Display git graph' },
+    { '<S-D-l>', '<cmd>Flog<cr>', desc = 'Display git graph' }
   },
   dependencies = {
     'tpope/vim-fugitive',
