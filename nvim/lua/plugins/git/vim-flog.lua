66c1e4cb50262cc4cbd73165dfc8cff01b284537
MrConnorKenway <auriaiur@outlook.com>
2025-01-22 12:46:49 +0800

    feat(vim-flog): add shortcut to display git log of visual range

--- nvim/lua/plugins/git/vim-flog.lua
+++ nvim/lua/plugins/git/vim-flog.lua
@@ -4,8 +4,10 @@ return {
     vim.g.flog_enable_extended_chars = true
   end,
   keys = {
-    { '<S-M-l>', '<cmd>Flog<cr>', desc = 'Display git graph' },
-    { '<S-D-l>', '<cmd>Flog<cr>', desc = 'Display git graph' }
+    { '<S-M-l>', mode = 'n', '<cmd>Flog<cr>',  desc = 'Display git graph' },
+    { '<S-M-l>', mode = 'v', ":'<,'>Flog<cr>", desc = 'Display git log of visual selected range', silent = true },
+    { '<S-D-l>', mode = 'n', '<cmd>Flog<cr>',  desc = 'Display git graph' },
+    { '<S-D-l>', mode = 'v', ":'<,'>Flog<cr>", desc = 'Display git log of visual selected range', silent = true }
   },
   dependencies = {
     'tpope/vim-fugitive',
