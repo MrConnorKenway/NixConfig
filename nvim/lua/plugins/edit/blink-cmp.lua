49842a7e3a42055acc21b2369ab85ddbc7033d0c
MrConnorKenway <auriaiur@outlook.com>
2025-01-23 11:20:48 +0800

    config(blink.cmp): remove appearance setting as it is the same as default

--- nvim/lua/plugins/edit/blink-cmp.lua
+++ nvim/lua/plugins/edit/blink-cmp.lua
@@ -42,12 +42,6 @@ return {
       }
     },
 
-    appearance = {
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono'
-    },
-
     -- default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, via `opts_extend`
     sources = {
