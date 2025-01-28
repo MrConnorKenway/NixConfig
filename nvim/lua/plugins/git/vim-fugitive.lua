e627b52511d8ca5f12e166a13f9b1ff499b5991a
MrConnorKenway <auriaiur@outlook.com>
2025-01-28 12:06:42 +0800

    feat(vim-fugitive): add shortcut to perform git commit

--- nvim/lua/plugins/git/vim-fugitive.lua
+++ nvim/lua/plugins/git/vim-fugitive.lua
@@ -1,13 +1,15 @@
 return {
   'tpope/vim-fugitive',
   keys = {
-    { 'gs',        '<cmd>G<cr>',               desc = 'Git status' },
-    { 'gv',        '<cmd>vertical G<cr>',      desc = 'Git status vertical' },
-    { 'gl',        '<cmd>G log --stat<cr>',    desc = 'Git log' },
-    { 'gu',        '<cmd>Git! push<cr>',       desc = 'Git push' },
-    { '<leader>d', '<cmd>Gdiffsplit<cr>',      desc = 'Git diff' },
-    { '<leader>D', '<cmd>Gvdiffsplit @:%<cr>', desc = 'Git diff with staged' },
-    { '<leader>g', ':G ',                      desc = 'Git cmdline' }
+    { 'gs',        '<cmd>G<cr>',                              desc = 'Git status' },
+    { 'gv',        '<cmd>vertical G<cr>',                     desc = 'Git status vertical' },
+    { 'gl',        '<cmd>G log --stat<cr>',                   desc = 'Git log' },
+    { 'gu',        '<cmd>Git! push<cr>',                      desc = 'Git push' },
+    { 'gcc',       '<cmd>tab G commit --verbose<cr>',         desc = 'Git commit' },
+    { 'gca',       '<cmd>tab G commit --verbose --amend<cr>', desc = 'Git commit' },
+    { '<leader>d', '<cmd>Gdiffsplit<cr>',                     desc = 'Git diff' },
+    { '<leader>D', '<cmd>Gvdiffsplit @:%<cr>',                desc = 'Git diff with staged' },
+    { '<leader>g', ':G ',                                     desc = 'Git cmdline' }
   },
   event = 'CmdlineEnter',
   init = function()
