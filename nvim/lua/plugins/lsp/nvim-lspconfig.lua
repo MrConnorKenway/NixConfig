b38b8229e77a39496f226ca664287a066915de43
MrConnorKenway <auriaiur@outlook.com>
2025-02-05 14:43:26 +0800

    fix(lspconfig): set the border of lsp floating window without using `vim.lsp.with`
    0.11 deprecates the use of `vim.lsp.with`, so we set the border with
    another hacky way.

--- nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -3,14 +3,13 @@ return {
   config = function()
     vim.api.nvim_create_autocmd('LspAttach', {
       callback = function(event)
-        vim.lsp.handlers['textDocument/hover'] =
-          vim.lsp.with(vim.lsp.handlers.hover, {
-            border = 'rounded',
-          })
-        vim.lsp.handlers['textDocument/signatureHelp'] =
-          vim.lsp.with(vim.lsp.handlers.signature_help, {
-            border = 'rounded',
-          })
+        local orig_util_open_floating_preview =
+          vim.lsp.util.open_floating_preview
+        function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+          opts = opts or {}
+          opts.border = 'rounded'
+          return orig_util_open_floating_preview(contents, syntax, opts, ...)
+        end
 
         vim.keymap.set('n', '<leader>i', function()
           vim.lsp.inlay_hint.enable(
