c6a3bd2ff10d28914c31e44d47f6493a9d9e06a3
MrConnorKenway <auriaiur@outlook.com>
2025-02-05 20:17:58 +0800

    feat(colorscheme): enable automatic theme changing

--- nvim/lua/plugins/ui/catppuccin.lua
+++ nvim/lua/plugins/ui/catppuccin.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   lazy = false,
   init = function()
-    vim.cmd.colorscheme('catppuccin-mocha')
+    vim.cmd.colorscheme('catppuccin')
   end,
   config = function()
     require('catppuccin').setup {
