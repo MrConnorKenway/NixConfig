f778d39035807944d28bc4ae933c2ba6260875dd
MrConnorKenway <auriaiur@outlook.com>
2025-02-05 14:41:24 +0800

    style(lspconfig): format nvim-lspconfig

--- nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -3,18 +3,27 @@ return {
   config = function()
     vim.api.nvim_create_autocmd('LspAttach', {
       callback = function(event)
-        vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-          border = 'rounded'
-        })
-        vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-          border = 'rounded'
-        })
+        vim.lsp.handlers['textDocument/hover'] =
+          vim.lsp.with(vim.lsp.handlers.hover, {
+            border = 'rounded',
+          })
+        vim.lsp.handlers['textDocument/signatureHelp'] =
+          vim.lsp.with(vim.lsp.handlers.signature_help, {
+            border = 'rounded',
+          })
 
         vim.keymap.set('n', '<leader>i', function()
-          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+          vim.lsp.inlay_hint.enable(
+            not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf })
+          )
         end, { desc = 'Toggle LSP inlay hint' })
         vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, { desc = 'LSP Rename' })
-        vim.keymap.set('n', 'g.', vim.lsp.buf.code_action, { desc = 'LSP code actions' })
+        vim.keymap.set(
+          'n',
+          'g.',
+          vim.lsp.buf.code_action,
+          { desc = 'LSP code actions' }
+        )
 
         vim.diagnostic.config({
           signs = {
@@ -23,18 +32,18 @@ return {
               [vim.diagnostic.severity.WARN] = '󰔶',
               [vim.diagnostic.severity.INFO] = '',
               [vim.diagnostic.severity.HINT] = '',
-            }
-          }
+            },
+          },
         })
-      end
+      end,
     })
 
     local lspconfig = require('lspconfig')
-    lspconfig.clangd.setup {
-      cmd = { 'clangd', '--header-insertion=never' }
-    }
-    lspconfig.nixd.setup {}
-    lspconfig.lua_ls.setup {}
-    lspconfig.basedpyright.setup {}
-  end
+    lspconfig.clangd.setup({
+      cmd = { 'clangd', '--header-insertion=never' },
+    })
+    lspconfig.nixd.setup({})
+    lspconfig.lua_ls.setup({})
+    lspconfig.basedpyright.setup({})
+  end,
 }
