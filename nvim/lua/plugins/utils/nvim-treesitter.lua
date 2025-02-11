ea2bfce612b709c4c85a159119cc13314b4665b5
MrConnorKenway <auriaiur@outlook.com>
2025-02-11 21:10:04 +0800

    refactor(nvim-treesitter): use minimum nvim-treesitter config

---
+++ nvim/lua/plugins/utils/nvim-treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  cmd = { 'TSInstall' },
+  ft = 'markdown',
+  config = function()
+    require('nvim-treesitter.install').ensure_installed {
+      'python',
+      'bash',
+      'cpp',
+    }
+  end,
+}
