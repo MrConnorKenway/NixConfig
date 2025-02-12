388cccf34510bceb9b16aa848091f4c464f1b0d4
MrConnorKenway <auriaiur@outlook.com>
2025-02-12 10:18:51 +0800

    chore(style): format blink-cmp.lua

--- nvim/lua/plugins/edit/blink-cmp.lua
+++ nvim/lua/plugins/edit/blink-cmp.lua
@@ -2,20 +2,8 @@
 return {
   'saghen/blink.cmp',
   lazy = false, -- lazy loading handled internally
-
-  -- use a release tag to download pre-built binaries
   version = 'v0.*',
-  -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
-
   opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- see the "default configuration" section below for full documentation on how to define
-    -- your own keymap.
     keymap = {
       ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
       ['<C-e>'] = { 'hide', 'fallback' },
@@ -29,7 +17,7 @@ return {
           end
         end,
         'snippet_forward',
-        'fallback'
+        'fallback',
       },
       ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
       ['<Up>'] = { 'select_prev', 'fallback' },
@@ -39,40 +27,35 @@ return {
       ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
       ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
       cmdline = {
-        preset = 'super-tab'
-      }
+        preset = 'super-tab',
+      },
     },
-
-    -- default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, via `opts_extend`
     sources = {
       default = { 'lsp', 'path' },
     },
-
     completion = {
       accept = { auto_brackets = { enabled = false } },
       menu = {
         winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None',
         draw = {
-          treesitter = { 'lsp' }
+          treesitter = { 'lsp' },
         },
-        border = 'rounded'
+        border = 'rounded',
       },
       documentation = {
         auto_show = true,
         window = {
           winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpDocCursorLine,Search:None',
-          border = 'rounded'
-        }
-      }
+          border = 'rounded',
+        },
+      },
     },
-
     signature = {
       enabled = true,
       window = {
         winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpDocCursorLine,Search:None',
-        border = 'rounded'
-      }
-    }
+        border = 'rounded',
+      },
+    },
   },
 }
