885b44fcadce279c57827df007b9e348e7075ede
MrConnorKenway <auriaiur@outlook.com>
2025-02-12 15:41:53 +0800

    feat(lazydev): disable lazydev if project root has `.luarc.json`

--- nvim/lua/plugins/lsp/lazydev.lua
+++ nvim/lua/plugins/lsp/lazydev.lua
@@ -5,6 +5,9 @@ return {
   opts = {
     library = {
       { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-    }
-  }
+    },
+    enabled = function(root_dir)
+      return not vim.uv.fs_stat(root_dir .. '/.luarc.json')
+    end,
+  },
 }
