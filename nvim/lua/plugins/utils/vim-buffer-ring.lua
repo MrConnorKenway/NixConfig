bc6bc3003a328e8a3b40f781519b6743a705acc8
MrConnorKenway <auriaiur@outlook.com>
2025-02-13 19:33:06 +0800

    chore(style): format vim-buffer-ring.lua and fix typos

--- nvim/lua/plugins/utils/vim-buffer-ring.lua
+++ nvim/lua/plugins/utils/vim-buffer-ring.lua
@@ -3,7 +3,17 @@ return {
   'landonb/vim-buffer-ring',
   event = 'VeryLazy',
   config = function()
-    vim.keymap.set('n', '[b', '<cmd>BufferRingReverse<cr>', { desc = 'Previous quickfix' })
-    vim.keymap.set('n', ']b', '<cmd>BufferRingForward<cr>', { desc = 'Next quickfix' })
-  end
+    vim.keymap.set(
+      'n',
+      '[b',
+      '<cmd>BufferRingReverse<cr>',
+      { desc = 'Previous buffer' }
+    )
+    vim.keymap.set(
+      'n',
+      ']b',
+      '<cmd>BufferRingForward<cr>',
+      { desc = 'Next buffer' }
+    )
+  end,
 }
