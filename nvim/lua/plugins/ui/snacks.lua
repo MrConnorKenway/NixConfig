a08f5eee637239b8b10b75692e3aac0cc0467143
MrConnorKenway <auriaiur@outlook.com>
2025-02-14 11:37:06 +0800

    feat(snacks): modify the provider of previous buffer in snacks' bufdelete

--- nvim/lua/plugins/ui/snacks.lua
+++ nvim/lua/plugins/ui/snacks.lua
@@ -44,6 +44,78 @@ local function snacks_terminal_toggle_or_focus()
   end
 end
 
+--- Delete a buffer:
+--- - either the current buffer if `buf` is not provided
+--- - or the buffer `buf` if it is a number
+--- - or every buffer for which `buf` returns true if it is a function
+---@param opts? number|snacks.bufdelete.Opts
+local function bufdelete(opts)
+  opts = opts or {}
+  opts = type(opts) == 'number' and { buf = opts } or opts
+  opts = type(opts) == 'function' and { filter = opts } or opts
+  ---@cast opts snacks.bufdelete.Opts
+
+  if type(opts.filter) == 'function' then
+    for _, b in ipairs(vim.tbl_filter(opts.filter, vim.api.nvim_list_bufs())) do
+      if vim.bo[b].buflisted then
+        bufdelete(
+          vim.tbl_extend('force', {}, opts, { buf = b, filter = false })
+        )
+      end
+    end
+    return
+  end
+
+  local buf = opts.buf or 0
+  buf = buf == 0 and vim.api.nvim_get_current_buf() or buf
+
+  vim.api.nvim_buf_call(buf, function()
+    if vim.bo.modified and not opts.force then
+      local ok, choice = pcall(
+        vim.fn.confirm,
+        ('Save changes to %q?'):format(vim.fn.bufname()),
+        '&Yes\n&No\n&Cancel'
+      )
+      if not ok or choice == 0 or choice == 3 then -- 0 for <Esc>/<C-c> and 3 for Cancel
+        return
+      end
+      if choice == 1 then -- Yes
+        vim.cmd.write()
+      end
+    end
+
+    for _, win in ipairs(vim.fn.win_findbuf(buf)) do
+      vim.api.nvim_win_call(win, function()
+        if
+          not vim.api.nvim_win_is_valid(win)
+          or vim.api.nvim_win_get_buf(win) ~= buf
+        then
+          return
+        end
+        -- Try using alternate buffer
+        local alt = vim.fn.bufnr('#')
+        if alt ~= buf and vim.fn.buflisted(alt) == 1 then
+          vim.api.nvim_win_set_buf(win, alt)
+          return
+        end
+
+        -- Try using previous buffer provided by vim-buffer-ring
+        local has_previous = pcall(vim.cmd, 'BufferRingReverse')
+        if has_previous and buf ~= vim.api.nvim_win_get_buf(win) then
+          return
+        end
+
+        -- Create new listed buffer
+        local new_buf = vim.api.nvim_create_buf(true, false)
+        vim.api.nvim_win_set_buf(win, new_buf)
+      end)
+    end
+    if vim.api.nvim_buf_is_valid(buf) then
+      pcall(vim.cmd, (opts.wipe and 'bwipeout! ' or 'bdelete! ') .. buf)
+    end
+  end)
+end
+
 ---@type LazyPluginSpec
 return {
   'folke/snacks.nvim',
@@ -240,9 +312,7 @@ return {
     },
     {
       '<leader>c',
-      function()
-        require('snacks.bufdelete').delete()
-      end,
+      bufdelete,
       desc = 'Close buffer',
     },
     {
@@ -422,9 +492,6 @@ return {
       enabled = true,
       top_down = false,
       timeout = 1000,
-    },
-    bufdelete = {
-      enabled = true,
     },
     picker = {
       enabled = true,
