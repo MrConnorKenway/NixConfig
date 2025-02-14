29c6b5f2848dee55077448fcff78e1670443e4ca
MrConnorKenway <auriaiur@outlook.com>
2025-02-14 15:37:11 +0800

    chore(style): format smart-splits.lua

--- nvim/lua/plugins/utils/smart-splits.lua
+++ nvim/lua/plugins/utils/smart-splits.lua
@@ -5,53 +5,69 @@ return {
     {
       '<C-h>',
       mode = { 'n', 'i', 't' },
-      function() require('smart-splits').move_cursor_left() end,
-      desc = 'Move cursort left'
+      function()
+        require('smart-splits').move_cursor_left()
+      end,
+      desc = 'Move cursort left',
     },
     {
       '<C-j>',
       mode = { 'n', 'i', 't' },
-      function() require('smart-splits').move_cursor_down() end,
-      desc = 'Move cursort down'
+      function()
+        require('smart-splits').move_cursor_down()
+      end,
+      desc = 'Move cursort down',
     },
     {
       '<C-k>',
       mode = { 'n', 'i', 't' },
-      function() require('smart-splits').move_cursor_up() end,
-      desc = 'Move cursort up'
+      function()
+        require('smart-splits').move_cursor_up()
+      end,
+      desc = 'Move cursort up',
     },
     {
       '<C-l>',
       mode = { 'n', 'i' },
-      function() require('smart-splits').move_cursor_right() end,
-      desc = 'Move cursort right'
+      function()
+        require('smart-splits').move_cursor_right()
+      end,
+      desc = 'Move cursort right',
     },
     {
       '<M-h>',
       mode = { 'n', 'i', 't' },
-      function() require('smart-splits').resize_left() end,
-      desc = 'Resize window left'
+      function()
+        require('smart-splits').resize_left()
+      end,
+      desc = 'Resize window left',
     },
     {
       '<M-j>',
       mode = { 'n', 'i', 't' },
-      function() require('smart-splits').resize_down() end,
-      desc = 'Resize window down'
+      function()
+        require('smart-splits').resize_down()
+      end,
+      desc = 'Resize window down',
     },
     {
       '<M-k>',
       mode = { 'n', 'i', 't' },
-      function() require('smart-splits').resize_up() end,
-      desc = 'Resize window up'
+      function()
+        require('smart-splits').resize_up()
+      end,
+      desc = 'Resize window up',
     },
     {
       '<M-l>',
       mode = { 'n', 'i', 't' },
-      function() require('smart-splits').resize_right() end,
-      desc = 'Resize window right'
-    }
+      function()
+        require('smart-splits').resize_right()
+      end,
+      desc = 'Resize window right',
+    },
   },
   opts = {
-    default_amount = 3
-  }
+    default_amount = 3,
+  },
 }
