6875629181bcc63f9dd2eebbc0b27f2355ccf363
MrConnorKenway <auriaiur@outlook.com>
2025-02-20 19:03:15 +0800

    tweak(auto-session): suppress home directory and use git branch name

--- nvim/lua/plugins/utils/auto-session.lua
+++ nvim/lua/plugins/utils/auto-session.lua
@@ -4,6 +4,8 @@ return {
   lazy = false,
   config = function()
     require('auto-session').setup {
+      suppressed_dirs = { '~' },
+      use_git_branch = true,
       session_lens = {
         load_on_setup = false
       }
