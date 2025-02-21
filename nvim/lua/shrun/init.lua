MrConnorKenway/NixConfig

commit a9e785629e7d180fed8ccc1d5b44a1dae3ac1ed5

fix(shrun): set task panel to empty buffer if no task remaining
Fixes: 436fee5 "refactor(shrun): remove unnecessary `nvim_win_set_buf` call"

nvim/lua/shrun/init.lua
@@ -704,6 +704,18 @@ local function new_sidebar_buffer()
     vim.bo[task_panel.sidebar_bufnr].modifiable = false
     vim.bo[task_panel.sidebar_bufnr].modified = false
 
+    if not next(all_tasks) then
+      if not vim.api.nvim_buf_is_valid(empty_task_output_buf) then
+        empty_task_output_buf = new_empty_buffer()
+      end
+      vim.wo[task_panel.task_output_winid].winfixbuf = false
+      vim.api.nvim_win_set_buf(
+        task_panel.task_output_winid,
+        empty_task_output_buf
+      )
+      vim.wo[task_panel.task_output_winid].winfixbuf = true
+    end
+
     if vim.api.nvim_buf_is_valid(task.buf_id) then
       vim.schedule(function()
         pcall(vim.api.nvim_buf_delete, task.buf_id, { force = true })

commit 6387355d85d377a489ed349359cf4327557a31d2

fix(shrun): prevent neovim from closing task output window when buffer is deleted
Fixes: a9e7856 "fix(shrun): set task panel to empty buffer if no task remaining"

nvim/lua/shrun/init.lua
@@ -704,17 +704,18 @@ local function new_sidebar_buffer()
     vim.bo[task_panel.sidebar_bufnr].modifiable = false
     vim.bo[task_panel.sidebar_bufnr].modified = false
 
-    if not next(all_tasks) then
-      if not vim.api.nvim_buf_is_valid(empty_task_output_buf) then
-        empty_task_output_buf = new_empty_buffer()
-      end
-      vim.wo[task_panel.task_output_winid].winfixbuf = false
-      vim.api.nvim_win_set_buf(
-        task_panel.task_output_winid,
-        empty_task_output_buf
-      )
-      vim.wo[task_panel.task_output_winid].winfixbuf = true
+    if not vim.api.nvim_buf_is_valid(empty_task_output_buf) then
+      empty_task_output_buf = new_empty_buffer()
     end
+    vim.wo[task_panel.task_output_winid].winfixbuf = false
+    -- Currently when buffer displayed in some window is deleted, that window
+    -- will be closed. Thus we set output window buffer to empty buffer to
+    -- prevent window closure.
+    vim.api.nvim_win_set_buf(
+      task_panel.task_output_winid,
+      empty_task_output_buf
+    )
+    vim.wo[task_panel.task_output_winid].winfixbuf = true
 
     if vim.api.nvim_buf_is_valid(task.buf_id) then
       vim.schedule(function()