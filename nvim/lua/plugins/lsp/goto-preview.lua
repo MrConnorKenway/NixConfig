e3f8c115a009a20d78e90f2f14a87f184dd581bb
MrConnorKenway <auriaiur@outlook.com>
2025-02-27 14:54:43 +0800

    fix(goto-preview): use snacks as fuzzy finder backend
    Waiting for https://github.com/rmagatti/goto-preview/issues/127 to
    resolve.

--- nvim/lua/plugins/lsp/goto-preview.lua
+++ nvim/lua/plugins/lsp/goto-preview.lua
@@ -12,7 +12,25 @@ return {
     {
       'gr',
       function()
-        require('goto-preview').goto_preview_references {}
+        local ok, snacks = pcall(require, 'snacks')
+        if not ok then
+          return
+        end
+
+        snacks.picker {
+          finder = 'lsp_references',
+          confirm = function(picker)
+            local selection = picker:current()
+            picker:close()
+
+            if selection ~= nil then
+              require('goto-preview.lib').open_floating_win(
+                vim.uri_from_fname(selection.file),
+                selection.pos
+              )
+            end
+          end,
+        }
       end,
       desc = 'Preview LSP references in popup',
     },
