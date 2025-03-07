MrConnorKenway/NixConfig

commit 21aaac5039571a258daef2b573f209eaa9e2bf02

feat(shrun): make focused task's command bold

nvim/lua/shrun/init.lua
@@ -118,6 +118,14 @@ local function highlight_focused()
     return
   end
 
+  vim.hl.range(
+    task_panel.sidebar_bufnr,
+    sidebar_focus_hl_ns,
+    'Bold',
+    { task_range.start_line - 1, 0 },
+    { task_range.start_line - 1, vim.o.columns }
+  )
+
   vim.api.nvim_buf_set_extmark(
     task_panel.sidebar_bufnr,
     sidebar_focus_hl_ns,

commit 48cb1aaecac3eefdf92cd8247a59aa17f0766ac6

tests(shrun): fix assertion after render content is changed
Fixes: 21aaac5 "feat(shrun): make focused task's command bold"

nvim/lua/shrun/init.lua
@@ -1531,13 +1531,26 @@ function M.test()
 
       -- focused task should be on the top
       abort_tests_if_not(task_panel.focused_task_range.start_line == 1)
-      abort_tests_if_not(#extmarks == 1)
-      abort_tests_if_not(
-        extmarks[1][2] == task_panel.focused_task_range.start_line - 1
-      )
-      abort_tests_if_not(
-        extmarks[1][4].end_row == task_panel.focused_task_range.end_line - 1
-      )
+      abort_tests_if_not(#extmarks == 2)
+      for _, extmark in ipairs(extmarks) do
+        if extmark[4].hl_group == 'Bold' then
+          abort_tests_if_not(
+            extmark[2] == task_panel.focused_task_range.start_line - 1
+          )
+          abort_tests_if_not(
+            extmark[4].end_row == task_panel.focused_task_range.start_line - 1
+          )
+        elseif extmark[4].line_hl_group == 'ShrunHighlightTaskFocus' then
+          abort_tests_if_not(
+            extmark[2] == task_panel.focused_task_range.start_line - 1
+          )
+          abort_tests_if_not(
+            extmark[4].end_row == task_panel.focused_task_range.end_line - 1
+          )
+        else
+          abort_tests_if_not(false)
+        end
+      end
     end,
     'normal x',
     ------------------ end test ------------------------------------------------