MrConnorKenway/NixConfig

commit dd503875b4ecb7b28236496bb8aba40414be1bec

feat(shrun): automatically change focus when switching task picker

nvim/lua/shrun/init.lua
@@ -1199,6 +1199,15 @@ function M.task_picker()
     format = 'text',
     formatters = { text = { ft = 'bash' } },
     preview = 'none',
+    on_change = function(_, item)
+      if not task_panel.sidebar_winid then
+        return
+      end
+
+      local range = task_panel.task_ranges[item.item]
+      task_panel.focused_task_range = range
+      highlight_focused()
+    end,
     confirm = function(picker, item)
       picker:close()
       if task_panel.sidebar_winid then

commit 3345f60df2accbd0fc12754414ac2ccb072c68ff

fix(shrun): set sidebar cursor when switching task picker
Fixes: dd503875 "feat(shrun): automatically change focus when switching task picker"

nvim/lua/shrun/init.lua
@@ -1260,6 +1260,10 @@ function M.task_picker()
 
       local range = task_panel.task_ranges[item.item]
       task_panel.focused_task_range = range
+      vim.api.nvim_win_set_cursor(
+        task_panel.sidebar_winid,
+        { range.start_line, 0 }
+      )
       highlight_focused()
     end,
     confirm = function(picker, item)