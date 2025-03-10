c370aa7a306a1f516dbfaf5da92e32cb0f9307d0
MrConnorKenway <auriaiur@outlook.com>
2025-03-10 20:51:34 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-sh <(curl https://mirrors.tuna.tsinghua.edu.cn/nix/latest/install)
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager # change 24.05 to match latest nixpkgs version
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz home-manager # change 24.05 to match latest nixpkgs version
 nix-channel --update
 nix-shell '<home-manager>' -A install
-home-manager switch --flake . --impure
+
+if [ "$(uname)" = "Darwin" ]; then
+	home-manager switch --flake .#darwin --impure
+else
+	home-manager switch --flake .#linux --impure
+fi
