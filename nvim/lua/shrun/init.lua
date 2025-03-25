MrConnorKenway/NixConfig

commit 23c1450e6f0ef3b0a4c4433a5abbae3a31d47310

fix(shrun): don't fire WinEnter and BufEnter when task panel is closing

nvim/lua/shrun/init.lua
@@ -338,6 +338,10 @@ local function new_task_output_window(buf_id)
 
   local autocmd_id = vim.api.nvim_create_autocmd('WinEnter', {
     callback = function()
+      -- We don't want to trigger WinEnter when task panel is closing
+      if not task_panel.sidebar_winid then
+        return
+      end
       if vim.api.nvim_get_current_win() == winid then
         save_original_winid()
       end
@@ -737,7 +741,12 @@ local function new_sidebar_buffer()
 
   vim.api.nvim_create_autocmd('BufEnter', {
     buffer = sidebar_bufnr,
-    callback = save_original_winid,
+    callback = function()
+      -- We don't want to trigger BufEnter when task panel is closing
+      if task_panel.task_output_winid then
+        save_original_winid()
+      end
+    end,
   })
 
   vim.api.nvim_create_autocmd('BufHidden', {

commit c39442a01cc24a13975f0fb0c9dfa8113f51b5e6

fix(shrun): save original window id in before displaying task panel
Fixes: 23c1450e "fix(shrun): don't fire WinEnter and BufEnter when task panel is closing"

nvim/lua/shrun/init.lua
@@ -825,6 +825,8 @@ function M.display_panel()
   -- Save exit window so that the following window creation won't overwrite `exit_win`
   local exit_win = task_panel.exit_win
 
+  original_winid = vim.api.nvim_get_current_win()
+
   -- Create sidebar window
   vim.cmd([[botright split]])
   local sidebar_winid = vim.api.nvim_get_current_win()