48c0b6962b41652a0e5141b50c1d54f56ea1d16d
MrConnorKenway <auriaiur@outlook.com>
2025-04-10 23:12:30 +0800

    feat(treesitter): add support for C++ static member function

--- nvim/queries/cpp/textobjects.scm
+++ nvim/queries/cpp/textobjects.scm
@@ -2,6 +2,9 @@
   declarator: (identifier) @function)
 
 (function_declarator
+  declarator: (field_identifier) @function)
+
+(function_declarator
   declarator: (parenthesized_declarator
     (pointer_declarator
       declarator: (field_identifier) @function)))
