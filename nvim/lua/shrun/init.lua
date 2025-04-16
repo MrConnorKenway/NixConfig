MrConnorKenway/NixConfig

commit 509e77086dc3068e431e576e3279b5b6dc103709

fix(shrun): correct the condition of whether sidebar buffer is empty

nvim/lua/shrun/init.lua
@@ -825,7 +825,7 @@ M.display_panel = function()
 
   if
     next(all_tasks)
-    and vim.api.nvim_buf_line_count(task_panel.sidebar_bufnr) == 0
+    and vim.api.nvim_buf_line_count(task_panel.sidebar_bufnr) <= 1
   then
     render_sidebar_from_scratch()
   end

commit 5640873f94c083b9d10fd0a54525077f2af64a15

fix(shrun): check if buffer is empty using buffer content as well as line number
Fixes: 509e7708 "fix(shrun): correct the condition of whether sidebar buffer is empty"

nvim/lua/shrun/init.lua
@@ -802,6 +802,11 @@ local function new_sidebar_buffer()
   return sidebar_bufnr
 end
 
+local function buf_is_empty(buf)
+  local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+  return #lines == 0 or (#lines == 1 and lines[1] == '')
+end
+
 function M.display_panel()
   if package.loaded.snacks.terminal then
     for _, term in ipairs(require('snacks.terminal').list()) do
@@ -815,10 +820,7 @@ function M.display_panel()
     task_panel.sidebar_bufnr = new_sidebar_buffer()
   end
 
-  if
-    next(all_tasks)
-    and vim.api.nvim_buf_line_count(task_panel.sidebar_bufnr) <= 1
-  then
+  if next(all_tasks) and buf_is_empty(task_panel.sidebar_bufnr) then
     render_sidebar_from_scratch()
   end
 