MrConnorKenway/NixConfig

commit d2ddf89afd08f5815d35044dc1fd15a5d166b168

refactor(shrun): use `desc_sorted_pairs` to iterate table by key's order

nvim/lua/shrun/init.lua
@@ -196,29 +196,44 @@ local function partial_render_sidebar(task)
   )
 end
 
+---@generic K, V
+---@param tbl table<K, V>
+---@return (fun(): K, V)
+local function desc_sorted_pairs(tbl)
+  local keys = {}
+  for k in pairs(tbl) do
+    keys[#keys + 1] = k
+  end
+  table.sort(keys, function(a, b) return a > b end)
+
+  local idx = 0
+  return function()
+    idx = idx + 1
+    if keys[idx] then
+      return keys[idx], tbl[keys[idx]]
+    end
+  end
+end
+
 ---caller should ensure that `task_panel` ~= nil
 local function render_sidebar_from_scratch()
   local lines = {}
   local highlights = {}
   local separator = string.rep(separator_stem, vim.o.columns)
-  local sorted_task_ids = {}
 
   task_panel.task_ranges = {}
   -- lua does not guarantee the order when iterating table, so we have to
   -- manually sort task id
-  for task_id, _ in pairs(all_tasks) do table.insert(sorted_task_ids, task_id) end
-  table.sort(sorted_task_ids)
-  for i = #sorted_task_ids, 1, -1 do
-    local task = all_tasks[i]
+  for task_id, task in desc_sorted_pairs(all_tasks) do
     local task_lines, task_highlights = render_task(task, #lines)
-    task_panel.task_ranges[i] = {
+    task_panel.task_ranges[task_id] = {
       start_line = #lines + 1,
       end_line = #lines + #task_lines,
       task_id = task.id,
     }
     vim.list_extend(lines, task_lines)
     vim.list_extend(highlights, task_highlights)
-    if i > 1 then
+    if task_id > 1 then
       table.insert(lines, separator)
       table.insert(highlights, { 'FloatBorder', #lines, 0, vim.o.columns })
     end
@@ -230,8 +245,8 @@ end
 ---@param lnum integer
 ---@return shrun.TaskRange?
 local function sidebar_get_task_range_from_line(lnum)
-  for _, task_range in pairs(task_panel.task_ranges) do
-    if task_range.start_line <= lnum and lnum <= task_range.end_line then
+  for _, task_range in desc_sorted_pairs(task_panel.task_ranges) do
+    if task_range.end_line >= lnum then
       return task_range
     end
   end

commit a5a77e86c76ea0bb5b44d40f4c4993b0549870a5

fix(shrun): fix a bug that causes separator being displayed even if there is only one task
To reproduce the bug, first make sure there are two tasks in sidebar,
delete the top task, and then reopen sidebar. Now there is a separator
below the only task. The root cause is: after 038af64 disables output
tail, when there is only one task, the buffer line count of sidebar is
1 instead of 2, thus when calling `display_panel`, `render_sidebar_from_scratch`
will be called, and since task id may be greater than 1, the separator
is drawed. To fix this issue, we should use the actual count of tasks to
decide if separator is needed.

Fixes: d2ddf89a "refactor(shrun): use `desc_sorted_pairs` to iterate table by key's order"

nvim/lua/shrun/init.lua
@@ -242,6 +242,7 @@ end
 
 local function render_sidebar_from_scratch()
   local lines = {}
+  local first = true
   local highlights = {}
   local separator_highlights = {}
   local separator = string.rep(separator_stem, vim.o.columns)
@@ -250,6 +251,17 @@ local function render_sidebar_from_scratch()
   -- lua does not guarantee the order when iterating table, so we have to
   -- manually sort task id
   for task_id, task in desc_sorted_pairs(all_tasks) do
+    if not first then
+      table.insert(lines, separator)
+      table.insert(
+        separator_highlights,
+        -- Don't use `vim.o.columns` because separator contains unicode characters
+        { 'FloatBorder', #lines, 0, separator:len() }
+      )
+    else
+      first = false
+    end
+
     local task_lines, task_highlights = task:render(#lines)
     task_panel.task_ranges[task_id] = {
       start_line = #lines + 1,
@@ -258,14 +270,6 @@ local function render_sidebar_from_scratch()
     }
     vim.list_extend(lines, task_lines)
     vim.list_extend(highlights, task_highlights)
-    if task_id > 1 then
-      table.insert(lines, separator)
-      table.insert(
-        separator_highlights,
-        -- Don't use `vim.o.columns` because separator contains unicode characters
-        { 'FloatBorder', #lines, 0, separator:len() }
-      )
-    end
   end
   vim.list_extend(highlights, separator_highlights)
 