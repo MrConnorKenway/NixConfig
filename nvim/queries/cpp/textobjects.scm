37176ee080f42d4dcb90f95509817ffcf81b862a
MrConnorKenway <auriaiur@outlook.com>
2025-04-18 14:02:36 +0800

    feat(treesitter): cpp add support for destructor

--- nvim/queries/cpp/textobjects.scm
+++ nvim/queries/cpp/textobjects.scm
@@ -1,5 +1,8 @@
 (function_declarator
-  declarator: (identifier) @function)
+  declarator: [
+    (identifier) @function
+    (destructor_name (identifier) @function)
+  ])
 
 (function_declarator
   declarator: (field_identifier) @function)
@@ -11,30 +14,49 @@
 
 (function_declarator
   declarator: [
-    (qualified_identifier name: (identifier) @function)
+    (qualified_identifier
+      name: [
+        (identifier) @function
+        (destructor_name (identifier) @function)
+      ])
     (qualified_identifier
       name: (qualified_identifier
-        name: (identifier) @function))
+        name: [
+          (identifier) @function
+          (destructor_name (identifier) @function)
+        ]))
     (qualified_identifier
       name: (qualified_identifier
         name: (qualified_identifier
-          name: (identifier) @function)))
+          name: [
+             (identifier) @function
+             (destructor_name (identifier) @function)
+          ])))
     (qualified_identifier
       name: (qualified_identifier
         name: (qualified_identifier
           name: (qualified_identifier
-            name: (identifier) @function))))
+            name: [
+               (identifier) @function
+               (destructor_name (identifier) @function)
+            ]))))
     (qualified_identifier
       name: (qualified_identifier
         name: (qualified_identifier
           name: (qualified_identifier
             name: (qualified_identifier
-              name: (identifier) @function)))))
+              name: [
+                 (identifier) @function
+                 (destructor_name (identifier) @function)
+              ])))))
     (qualified_identifier
       name: (qualified_identifier
         name: (qualified_identifier
           name: (qualified_identifier
             name: (qualified_identifier
               name: (qualified_identifier
-                name: (identifier) @function))))))
+                name: [
+                   (identifier) @function
+                   (destructor_name (identifier) @function)
+                ]))))))
   ])
