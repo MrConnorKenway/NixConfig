c72148ed2c60c72565c36b24f7faf7c59c83251a
MrConnorKenway <auriaiur@outlook.com>
2025-04-21 17:55:33 +0800

    chore(style): format gitsigns.lua

--- nvim/lua/plugins/git/gitsigns.lua
+++ nvim/lua/plugins/git/gitsigns.lua
@@ -5,15 +5,15 @@ return {
     local gitsigns = require('gitsigns')
     gitsigns.setup {
       signs = {
-        changedelete = { text = '┃' }
+        changedelete = { text = '┃' },
       },
       signs_staged = {
-        changedelete = { text = '┃' }
+        changedelete = { text = '┃' },
       },
       sign_priority = 100,
       preview_config = {
-        border = 'rounded'
-      }
+        border = 'rounded',
+      },
     }
     vim.keymap.set('n', ']c', function()
       if vim.api.nvim_buf_get_name(0):match('fugitive://') then
@@ -29,13 +29,36 @@ return {
         gitsigns.nav_hunk('prev', { target = 'all' })
       end
     end, { desc = 'Go to previous git change' })
-    vim.keymap.set('n', '<leader>u', gitsigns.reset_hunk, { desc = 'Git reset hunk' })
-    vim.keymap.set('v', '<leader>u', ":'<,'>Gitsigns reset_hunk<cr>",
-      { desc = 'Git reset hunk of visual selected range', silent = true })
-    vim.keymap.set('n', '<leader>b', gitsigns.blame_line, { desc = 'Git blame inline' })
+    vim.keymap.set(
+      'n',
+      '<leader>u',
+      gitsigns.reset_hunk,
+      { desc = 'Git reset hunk' }
+    )
+    vim.keymap.set(
+      'v',
+      '<leader>u',
+      ":'<,'>Gitsigns reset_hunk<cr>",
+      { desc = 'Git reset hunk of visual selected range', silent = true }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>b',
+      gitsigns.blame_line,
+      { desc = 'Git blame inline' }
+    )
     vim.keymap.set('n', 'ga', gitsigns.stage_hunk, { desc = 'Git stage hunk' })
-    vim.keymap.set('v', 'ga', ":'<,'>Gitsigns stage_hunk<cr>",
-      { desc = 'Git stage hunk of visual selected range', silent = true })
-    vim.keymap.set('n', 'gi', gitsigns.preview_hunk_inline, { desc = 'Git preview hunk inline' })
-  end
+    vim.keymap.set(
+      'v',
+      'ga',
+      ":'<,'>Gitsigns stage_hunk<cr>",
+      { desc = 'Git stage hunk of visual selected range', silent = true }
+    )
+    vim.keymap.set(
+      'n',
+      'gi',
+      gitsigns.preview_hunk_inline,
+      { desc = 'Git preview hunk inline' }
+    )
+  end,
 }
