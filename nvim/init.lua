MrConnorKenway/NixConfig

commit 7048cf490dcca2cd00f956e0dcaeb33f6d311e68

feat: prompt user to confirm exit if there is running job

nvim/init.lua
@@ -170,14 +170,27 @@ local function is_last_window()
 end
 
 local function confirm_to_exit()
-  if require('shrun').nr_tasks_by_status()['RUNNING'] > 0 then
+  local jobs = vim.tbl_filter(function(chan)
+    return chan.stream == 'job'
+      and chan.id ~= require('shrun').get_shell_job()
+      and chan.pty ~= ''
+  end, vim.api.nvim_list_chans())
+
+  if #jobs > 0 then
     local choice = vim.fn.confirm(
-      'Are you asure to exit? There are running tasks.',
+      'Are you asure to exit? There are running jobs.',
       '&Yes\n&No',
       2,
       'Question'
     )
-    return choice == 1
+    if choice == 1 then
+      for _, job in ipairs(jobs) do
+        vim.fn.jobstop(job.id)
+      end
+      return true
+    else
+      return false
+    end
   end
 
   return true

nvim/lua/shrun/init.lua
@@ -1096,6 +1096,10 @@ local shell_buf
 local shell_job
 local shell_win
 
+function M.get_shell_job()
+  return shell_job
+end
+
 function M.launch_shell()
   local shell = vim.o.shell:gsub('(.*)/', '')
   local shell_args

commit 9215aafa0323e25bbc4753d055808b14d8d14bb2

fix(shrun): stop shrun shell when user confirms to exit
Fixes: 7048cf4 "feat: prompt user to confirm exit if there is running job"

nvim/init.lua
@@ -177,10 +177,9 @@ local function is_current_window_last()
 end
 
 local function confirm_to_exit()
+  local shrun_shell = require('shrun').get_shell_job()
   local jobs = vim.tbl_filter(function(chan)
-    return chan.stream == 'job'
-      and chan.id ~= require('shrun').get_shell_job()
-      and chan.pty ~= ''
+    return chan.stream == 'job' and chan.id ~= shrun_shell and chan.pty ~= ''
   end, vim.api.nvim_list_chans())
 
   if #jobs > 0 then
@@ -194,12 +193,14 @@ local function confirm_to_exit()
       for _, job in ipairs(jobs) do
         vim.fn.jobstop(job.id)
       end
-      return true
     else
       return false
     end
   end
 
+  if shrun_shell then
+    vim.fn.jobstop(shrun_shell)
+  end
   return true
 end
 