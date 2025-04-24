MrConnorKenway/NixConfig

commit 36164f435970d404b9e69315e1f0fa30775a9c69

fix(git): do not redraw statusline of invalid buffer
Fixes: f4e18f2 "feat(git): redraw statusline for every attached gitsigns buffer when registered event triggers"

nvim/lua/plugins/ui/heirline.lua
@@ -362,7 +362,11 @@ return {
           local wins = vim.api.nvim_tabpage_list_wins(0)
           for _, win in ipairs(wins) do
             local bufnr = vim.api.nvim_win_get_buf(win)
-            if cache[bufnr] and not redrawing[bufnr] then
+            if
+              cache[bufnr]
+              and vim.api.nvim_buf_is_valid(bufnr)
+              and not redrawing[bufnr]
+            then
               redrawing[bufnr] = true
               vim.schedule(function()
                 vim.api.nvim__redraw { buf = bufnr, statusline = true }

commit 1ce64ee1c626723a79002448ab693944074c32e1

fix(git): validate buffer inside scheduled callback
Fixes: 36164f4 "fix(git): do not redraw statusline of invalid buffer"

nvim/lua/plugins/ui/heirline.lua
@@ -361,14 +361,12 @@ return {
           local wins = vim.api.nvim_tabpage_list_wins(0)
           for _, win in ipairs(wins) do
             local bufnr = vim.api.nvim_win_get_buf(win)
-            if
-              cache[bufnr]
-              and vim.api.nvim_buf_is_valid(bufnr)
-              and not redrawing[bufnr]
-            then
+            if cache[bufnr] and not redrawing[bufnr] then
               redrawing[bufnr] = true
               vim.schedule(function()
-                vim.api.nvim__redraw { buf = bufnr, statusline = true }
+                if vim.api.nvim_buf_is_valid(bufnr) then
+                  vim.api.nvim__redraw { buf = bufnr, statusline = true }
+                end
                 redrawing[bufnr] = false
               end)
             end