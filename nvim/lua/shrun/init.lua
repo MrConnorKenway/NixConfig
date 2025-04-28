MrConnorKenway/NixConfig

commit 21aaac5039571a258daef2b573f209eaa9e2bf02

feat(shrun): make focused task's command bold

nvim/lua/shrun/init.lua
@@ -118,6 +118,14 @@ local function highlight_focused()
     return
   end
 
+  vim.hl.range(
+    task_panel.sidebar_bufnr,
+    sidebar_focus_hl_ns,
+    'Bold',
+    { task_range.start_line - 1, 0 },
+    { task_range.start_line - 1, vim.o.columns }
+  )
+
   vim.api.nvim_buf_set_extmark(
     task_panel.sidebar_bufnr,
     sidebar_focus_hl_ns,

commit fc2500f6c08a0f0540af5449589f2a5da7924cd8

fix(shrun): make whole line of task command bold
Fixes: 21aaac5 "feat(shrun): make focused task's command bold"

nvim/lua/shrun/init.lua
@@ -130,7 +130,7 @@ local function highlight_focused()
     sidebar_focus_hl_ns,
     'Bold',
     { task_range.start_line - 1, 0 },
-    { task_range.start_line - 1, vim.o.columns }
+    { task_range.start_line - 1, -1 }
   )
 
   vim.api.nvim_buf_set_extmark(