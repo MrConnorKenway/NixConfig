2378c23572fd5c307ab23193477cb0d31821e456
MrConnorKenway <auriaiur@outlook.com>
2025-05-15 10:40:32 +0800

    tweak(vim-illuminate): disable illuminate on special buffer

--- nvim/lua/plugins/lsp/vim-illuminate.lua
+++ nvim/lua/plugins/lsp/vim-illuminate.lua
@@ -6,6 +6,9 @@ return {
     require('illuminate').configure {
       modes_allowlist = { 'n' },
       providers = { 'lsp', 'regex' },
+      should_enable = function(bufnr)
+        return vim.bo[bufnr].buftype == ''
+      end,
     }
     require('illuminate.config').get_raw().large_file_cutoff = nil
   end,
