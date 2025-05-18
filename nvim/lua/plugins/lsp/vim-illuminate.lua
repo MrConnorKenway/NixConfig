b24fa76d1ff6b9e79e25d519b11ad208a19ec320
MrConnorKenway <auriaiur@outlook.com>
2025-05-18 21:26:23 +0800

    tweak(vim-illuminate): re-enable regex with updated vim-illuminate

--- nvim/lua/plugins/lsp/vim-illuminate.lua
+++ nvim/lua/plugins/lsp/vim-illuminate.lua
@@ -1,11 +1,14 @@
 ---@type LazyPluginSpec
 return {
   'MrConnorKenway/vim-illuminate',
-  event = 'LspAttach',
   config = function()
     require('illuminate').configure {
       modes_allowlist = { 'n' },
-      providers = { 'lsp' },
+      providers = { 'lsp', 'regex' },
+      should_enable = function(bufnr)
+        return vim.bo[bufnr].buftype == ''
+          and not vim.bo[bufnr].filetype:find('snacks_')
+      end,
     }
     require('illuminate.config').get_raw().large_file_cutoff = nil
   end,
