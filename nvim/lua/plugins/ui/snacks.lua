MrConnorKenway/NixConfig

commit 89c894bc34477efcfbb4f49fcd6d7738525cf715

refactor(snacks.picker): use '+++ b/' to find git diff file name more precisely

nvim/lua/plugins/ui/snacks.lua
@@ -420,8 +420,8 @@ return {
             finder(function(proc_item)
               local diff_text = proc_item.text
 
-              if diff_text:sub(1, 4) == 'diff' then
-                file_name = diff_text:match('^diff .* a/.* b/(.*)$')
+              if diff_text:sub(1, 6) == '+++ b/' then
+                file_name = diff_text:sub(7)
                 bufnr = attached_bufnr[file_name]
                 if bufnr then
                   use_gitsigns = true

commit b07fcf625c146fd82dec47f7b3472cf2039aab55

fix(snacks): skip '--- a/' when parsing git diff header
Fixes: 89c894bc "refactor(snacks.picker): use '+++ b/' to find git diff file name more precisely"

nvim/lua/plugins/ui/snacks.lua
@@ -421,17 +421,27 @@ return {
             finder(function(proc_item)
               local diff_text = proc_item.text
 
-              if diff_text:sub(1, 6) == '+++ b/' then
-                file_name = diff_text:sub(7)
-                bufnr = attached_bufnr[file_name]
-                if bufnr then
-                  use_gitsigns = true
-                else
-                  use_gitsigns = false
-                  in_hunk = false
+              if diff_text:sub(1, 4) == 'diff' then
+                in_hunk = false
+                return
+              end
+
+              if not in_hunk then
+                if diff_text:sub(1, 6) == '--- a/' then
+                  return
                 end
 
-                return
+                if diff_text:sub(1, 6) == '+++ b/' then
+                  file_name = diff_text:sub(7)
+                  bufnr = attached_bufnr[file_name]
+                  if bufnr then
+                    use_gitsigns = true
+                  else
+                    use_gitsigns = false
+                  end
+
+                  return
+                end
               end
 
               if use_gitsigns then