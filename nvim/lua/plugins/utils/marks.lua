5bb52099ba888b67638fa8da34edc2b3e52b814d
MrConnorKenway <auriaiur@outlook.com>
2025-05-20 00:03:20 +0800

    tweak(marks.nvim): add shortcut to pick marks using Snacks

--- nvim/lua/plugins/utils/marks.lua
+++ nvim/lua/plugins/utils/marks.lua
@@ -2,5 +2,15 @@
 return {
   'chentoast/marks.nvim',
   event = 'VeryLazy',
+  keys = {
+    {
+      '<leader>m',
+      function()
+        require('marks').mark_state:all_to_list('quickfixlist')
+        require('snacks.picker').qflist()
+      end,
+      desc = 'Show all marks in all buffers',
+    },
+  },
   opts = {},
 }
