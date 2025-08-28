MrConnorKenway/NixConfig

commit 7338c19a82ddc696bcdd8450e2164db65998a0b0

feat(git): display repo name if not equal to current working directory

nvim/lua/plugins/ui/heirline.lua
@@ -380,6 +380,14 @@ return {
 
       {
         provider = function(self)
+          local repo = vim.fs.basename(self.status_dict.root)
+          local cwd = vim.fs.basename(vim.uv.cwd())
+          return repo ~= cwd and ' ' .. repo .. ' '
+        end,
+        hl = { bold = true },
+      },
+      {
+        provider = function(self)
           local head = self.status_dict.head
           return (head:match('^[0-9a-f]+$') and ' ' or ' ') .. head
         end,

commit c67dcb0de0b99b1128d17c40fe0504b4f382ff45

fix(git): early return if git root is not ready
Fixes: 7338c19a "feat(git): display repo name if not equal to current working directory"

nvim/lua/plugins/ui/heirline.lua
@@ -368,6 +368,9 @@ return {
 
       {
         provider = function(self)
+          if not self.status_dict.root then
+            return
+          end
           local repo = vim.fs.basename(self.status_dict.root)
           local cwd = vim.fs.basename(vim.uv.cwd())
           return repo ~= cwd and ' ' .. repo .. ' '