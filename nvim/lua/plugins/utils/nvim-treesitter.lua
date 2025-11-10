MrConnorKenway/NixConfig

commit b39c78dadf6eefa8b21779c9f1f8a7fcd537d7c1

feat(treesitter): add shortcut to navigate to function name

nvim/lua/plugins/utils/nvim-treesitter.lua
@@ -1,3 +1,58 @@
+vim.keymap.set('n', '[m', function()
+  local buf = vim.api.nvim_get_current_buf()
+  local win = vim.api.nvim_get_current_win()
+  local cursor_row, _ = unpack(vim.api.nvim_win_get_cursor(win))
+  local ok, parser = pcall(vim.treesitter.get_parser, buf)
+  if ok and parser then
+    parser:parse(true)
+    local nearest_prev_func_start_row = 0
+    local nearest_prev_func_start_col = 0
+
+    parser:for_each_tree(function(tstree, tree)
+      if not tstree then
+        return
+      end
+
+      local query = vim.treesitter.query.get(tree:lang(), 'textobjects')
+      if not query then
+        return
+      end
+
+      local func_id
+
+      for i, capture_name in ipairs(query.captures) do
+        if capture_name == 'function' then
+          func_id = i
+          break
+        end
+      end
+
+      for capture_id, node, _ in query:iter_captures(tstree:root(), buf) do
+        if capture_id == func_id then
+          local func_start_row, func_start_col, _, _ = node:range()
+          func_start_row = func_start_row + 1
+          if
+            func_start_row < cursor_row
+            and cursor_row - func_start_row
+              < cursor_row - nearest_prev_func_start_row
+          then
+            nearest_prev_func_start_row = func_start_row
+            nearest_prev_func_start_col = func_start_col
+          end
+        end
+      end
+
+      if nearest_prev_func_start_row ~= 0 then
+        vim.cmd("normal! m'") -- add to jump list
+        vim.api.nvim_win_set_cursor(
+          win,
+          { nearest_prev_func_start_row, nearest_prev_func_start_col }
+        )
+      end
+    end)
+  end
+end)
+
 ---@type LazyPluginSpec
 return {
   'nvim-treesitter/nvim-treesitter',

nvim/queries/c/textobjects.scm
@@ -1,0 +1,7 @@
+(function_declarator
+  declarator: (identifier) @function)
+
+(function_declarator
+  declarator: (parenthesized_declarator
+    (pointer_declarator
+      declarator: (field_identifier) @function)))

nvim/queries/cpp/textobjects.scm
@@ -1,0 +1,7 @@
+(function_declarator
+  declarator: (identifier) @function)
+
+(function_declarator
+  declarator: (parenthesized_declarator
+    (pointer_declarator
+      declarator: (field_identifier) @function)))

nvim/queries/lua/textobjects.scm
@@ -1,0 +1,12 @@
+(function_declaration
+  name: [
+    (identifier) @function
+    (dot_index_expression
+      field: (identifier) @function)
+  ])
+
+(function_definition
+  !name) @function
+
+(function_declaration
+  !name) @function

nvim/queries/python/textobjects.scm
@@ -1,0 +1,2 @@
+(function_definition
+  name: (identifier) @function)

commit 7887f6e3a5d9eee82185b74f3e069c32c7fd108e

fix(treesitter): move cursor only once after checking all trees
Fixes: b39c78da "feat(treesitter): add shortcut to navigate to function
name"

nvim/lua/plugins/utils/nvim-treesitter.lua
@@ -63,15 +63,15 @@ vim.keymap.set('n', '[m', function()
           end
         end
       end
-
-      if nearest_prev_func_start_row ~= 0 then
-        vim.cmd("normal! m'") -- add to jump list
-        vim.api.nvim_win_set_cursor(
-          win,
-          { nearest_prev_func_start_row, nearest_prev_func_start_col }
-        )
-      end
     end)
+
+    if nearest_prev_func_start_row ~= 0 then
+      vim.cmd("normal! m'") -- add to jump list
+      vim.api.nvim_win_set_cursor(
+        win,
+        { nearest_prev_func_start_row, nearest_prev_func_start_col }
+      )
+    end
   end
 end)
 