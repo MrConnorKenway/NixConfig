64da351871fca3a55fc49c31b23b624dbbbf2a14
MrConnorKenway <auriaiur@outlook.com>
2025-12-10 00:12:58 +0800

    Fix some bugs in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,11 +8,15 @@ else
 	sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
 fi
 
-echo substituters = https://mirrors.ustc.edu.cn/nix-channels/store https://cache.nixos.org/ >> $HOME/.config/nix/nix.conf
+mkdir -p $HOME/.config/nix
+
+echo substituters = https://mirrors.ustc.edu.cn/nix-channels/store https://cache.nixos.org/ > $HOME/.config/nix/nix.conf
 echo experimental-features = nix-command flakes >> $HOME/.config/nix/nix.conf
 
 if [ "$os" = "Darwin" ]; then
+	export PATH=/nix/var/nix/profiles/default/bin:$PATH
 	nix run home-manager/master -- switch --flake .#darwin --impure
 else
+	export PATH=$HOME/.nix-profile/bin:$PATH
 	nix run home-manager/master -- switch --flake .#linux --impure
 fi
