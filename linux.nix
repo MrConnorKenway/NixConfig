fb6682b60291bed1aa6f63a54e7d58cd26aac137
MrConnorKenway <auriaiur@outlook.com>
2026-03-03 14:42:29 +0800

    nix: source "nix.sh" on linux

--- linux.nix
+++ linux.nix
@@ -6,4 +6,12 @@
       pahole
     ];
   };
+
+  programs.zsh = {
+    envExtra = ''
+      if [[ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]]; then
+        . $HOME/.nix-profile/etc/profile.d/nix.sh
+      fi
+    '';
+  };
 }
