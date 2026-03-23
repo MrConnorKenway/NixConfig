134be366ecdec3556a30a48fd60ff2f7c948c1cb
MrConnorKenway <auriaiur@outlook.com>
2026-03-23 22:26:56 +0800

    refactor(catppuccin): directly load colorscheme

--- nvim/lua/plugins/ui/catppuccin.lua
+++ nvim/lua/plugins/ui/catppuccin.lua
@@ -4,9 +4,6 @@ return {
   name = 'catppuccin-colorscheme',
   priority = 1000,
   lazy = false,
-  init = function()
-    vim.cmd.colorscheme('catppuccin')
-  end,
   config = function()
     require('catppuccin').setup {
       custom_highlights = function(C)
@@ -55,5 +52,6 @@ return {
         blink_cmp = true,
       },
     }
+    require('catppuccin').load()
   end,
 }
