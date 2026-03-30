1054bd19b2d76be2683257896f8143face178f0d
MrConnorKenway <auriaiur@outlook.com>
2026-03-30 11:43:52 +0800

    feat(lsp): show lsp progress in terminal progress bar

--- nvim/init.lua
+++ nvim/init.lua
@@ -599,48 +599,24 @@ vim.api.nvim_create_autocmd({ 'WinEnter', 'BufWinEnter' }, {
   end,
 })
 
----@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
-local progress = vim.defaulttable()
 vim.api.nvim_create_autocmd('LspProgress', {
-  ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
   callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: 'begin' | 'report' | 'end'}]]
-    if not client or type(value) ~= 'table' then
+    local value = ev.data.params.value or {}
+    if not value.kind then
       return
     end
-    local p = progress[client.id]
 
-    for i = 1, #p + 1 do
-      if i == #p + 1 or p[i].token == ev.data.params.token then
-        p[i] = {
-          token = ev.data.params.token,
-          msg = ('[%3d%%] %s%s'):format(
-            value.kind == 'end' and 100 or value.percentage or 100,
-            value.title or '',
-            value.message and (' **%s**'):format(value.message) or ''
-          ),
-          done = value.kind == 'end',
-        }
-        break
-      end
+    local status = value.kind == 'end' and 0 or 1
+    local percent = value.percentage or 0
+
+    local osc_seq = string.format('\27]9;4;%d;%d\a', status, percent)
+
+    if os.getenv('TMUX') then
+      osc_seq = string.format('\27Ptmux;\27%s\27\\', osc_seq)
     end
 
-    local msg = {} ---@type string[]
-    progress[client.id] = vim.tbl_filter(function(v)
-      return table.insert(msg, v.msg) or not v.done
-    end, p)
-
-    local spinner =
-      { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
-    vim.notify(table.concat(msg, '\n'), vim.log.levels.INFO, {
-      id = 'lsp_progress',
-      title = client.name,
-      opts = function(notif)
-        notif.icon = #progress[client.id] == 0 and ' '
-          or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-      end,
-    })
+    io.stdout:write(osc_seq)
+    io.stdout:flush()
   end,
 })
 
